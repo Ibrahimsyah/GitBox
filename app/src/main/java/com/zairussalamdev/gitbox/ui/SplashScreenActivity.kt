2d643757a8a944d78efd9c244f56dd4075aaa930
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-10 11:27:44 +0700

    Add handler for SplashScreen

--- app/src/main/java/com/zairussalamdev/gitbox/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/zairussalamdev/gitbox/ui/SplashScreenActivity.kt
@@ -1,12 +1,24 @@
 package com.zairussalamdev.gitbox.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.zairussalamdev.gitbox.R
+import com.zairussalamdev.gitbox.ui.main.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    companion object {
+        const val SPLASH_SCREEN_DELAY_MILS = 2500L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, SPLASH_SCREEN_DELAY_MILS)
     }
 }
