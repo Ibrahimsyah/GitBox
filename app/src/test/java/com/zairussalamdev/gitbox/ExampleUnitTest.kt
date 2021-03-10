91f9ec4d40429d926d5b2c2e5f8a1df0388c77e3
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-10 21:36:19 +0700

    Remove unused resources

--- app/src/test/java/com/zairussalamdev/gitbox/ExampleUnitTest.kt
+++ app/src/test/java/com/zairussalamdev/gitbox/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.zairussalamdev.gitbox
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
