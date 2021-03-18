f4a9fb6795264441b6e32b92b61c5f0c30ec3d41
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-18 22:09:28 +0700

    Change detail activity layout

--- app/src/main/java/com/zairussalamdev/gitbox/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/zairussalamdev/gitbox/ui/detail/DetailActivity.kt
@@ -3,6 +3,8 @@ package com.zairussalamdev.gitbox.ui.detail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.google.android.material.tabs.TabLayoutMediator
 import com.zairussalamdev.gitbox.databinding.ActivityDetailBinding
 
@@ -22,20 +24,20 @@ class DetailActivity : AppCompatActivity() {
 
         username?.let {
             viewModel.getUserDetail(username).observe(this, {
-//                with(binding) {
-//                    userName.text = it.name
-//                    userUsername.text = it.username
-//                    userRepository.text = it.repository.toString()
-//                    userFollower.text = it.followers.toString()
-//                    userFollowing.text = it.following.toString()
-//                    userCompany.text = it.company
-//                    userLocation.text = it.location
-//
-//                    userImage.load(it.avatar) {
-//                        crossfade(true)
-//                        transformations(CircleCropTransformation())
-//                    }
-//                }
+                with(binding) {
+                    userName.text = it.name
+                    userUsername.text = it.username
+                    userRepository.text = it.repository.toString()
+                    userFollower.text = it.followers.toString()
+                    userFollowing.text = it.following.toString()
+                    userCompany.text = it.company
+                    userLocation.text = it.location
+
+                    userImage.load(it.avatar) {
+                        crossfade(true)
+                        transformations(CircleCropTransformation())
+                    }
+                }
             })
         }
 
